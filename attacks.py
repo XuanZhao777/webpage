9cf354165542db28aeb08bedd53155c102edd320
XuanZhao777 <149707203+XuanZhao777@users.noreply.github.com>
2023-12-08 17:17:43 +0900

    Update attacks.py

--- attacks.py
+++ attacks.py
@@ -1,29 +1,42 @@
-# attacks.py
 import torch
 import torch.nn as nn
 
-
 class FGSMAttack:
-    def __init__(self, model, epsilon):
+    def __init__(self, model, epsilon, _type='linf', num_iters=10, alpha=1.0):
         self.model = model
         self.epsilon = epsilon
+        self._type = _type
+        self.num_iters = num_iters
+        self.alpha = alpha
 
-    def perturb(self, x, y):
-        # FGSM Attack
+    def perturb(self, original_images, labels):
+        device = original_images.get_device()
+
+        x = original_images.clone()
         x.requires_grad = True
-        outputs = self.model(x)
 
-        # Extract the actual tensor from the tuple
-        if isinstance(outputs, tuple):
-            outputs = outputs[0]
+        for _ in range(self.num_iters):
+            with torch.enable_grad():
+                outputs, _, _, _ = self.model(x, is_eval=True)
+                cls_loss = nn.CrossEntropyLoss()(outputs, labels)
 
-        # Ensure the target tensor y is a 1D tensor
-        if y.dim() > 1:
-            y = y.squeeze(dim=1)
+            grad_outputs = None
+            grads = torch.autograd.grad(cls_loss, x, grad_outputs=grad_outputs, only_inputs=True)[0]
 
-        loss = nn.CrossEntropyLoss()(outputs, y)
-        gradient = torch.autograd.grad(loss, x)[0]
-        x_adv = x + self.epsilon * gradient.sign()
-        x_adv = torch.clamp(x_adv, 0, 1)
-        return x_adv
+            x.data += self.alpha * torch.sign(grads.data)
+            x = project(x, original_images, self.epsilon, self._type)
+            x.clamp_(0, 1)  # Assuming the input data is normalized to [0, 1]
 
+        return x.detach()
+
+def project(x, original_x, epsilon, _type='linf'):
+    if _type == 'linf':
+        max_x = original_x + epsilon
+        min_x = original_x - epsilon
+
+        x = torch.max(torch.min(x, max_x), min_x)
+
+    else:
+        raise NotImplementedError
+
+    return x
